b207c5b8055c5362286b3215a7d2a0e174afbb5b
Jarod Davies <balancejackfireblade@gmail.com>
2023-09-26 13:53:06 -0400

    Front End: Completely removed old submit code

--- js/code.js
+++ js/code.js
@@ -309,8 +309,6 @@ function searchContact() {
 }
 
 function addContactPanel() {
-	contactForm = document.getElementById("contact-form");
-	contactForm.setAttribute("onsubmit", "addContact();return false");
 	contactPanel = document.getElementById("contact-panel");
 	contactPanel.style.display = "block";
 	sidebarTitle = document.getElementById("sidebar-title");
@@ -329,8 +327,6 @@ function addContactPanel() {
 }
 
 function editContactPanel(editButton) {
-	contactForm = document.getElementById("contact-form");
-
 	contactPanel = document.getElementById("contact-panel");
 	contactPanel.style.display = "block";
 	sidebarTitle = document.getElementById("sidebar-title");
